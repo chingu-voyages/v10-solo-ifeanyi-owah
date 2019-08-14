90980a948bfd4773def47809a37585d64b38d449
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2019-08-14 00:43:09 -0400

    updated images with  transition

--- my-ecommerce-app/src/components/MenuItem/MenuItem.jsx
+++ my-ecommerce-app/src/components/MenuItem/MenuItem.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './MenuItem.scss';
 
 
-const MenuItem = ({title, imgUrl, size}) => (
-    <div style={{backgroundImage:`url(${imgUrl}`}} 
-    className={`${size} Menu-item`}>
-        <div className='content'>
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imgUrl, size }) => (
+   
+        <div className={`${size} Menu-item`}>
+                <div style={{ backgroundImage: `url(${imgUrl}` }} 
+                className='Menu-background-image'>
+
+                </div>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
-    </div>
+   
+
 );
 
 
